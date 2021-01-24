1eb6efdc21b12488ce200d76171c5259f44b924c
Casper Sparre <reptail@gmail.com>
2021-01-24 13:20:20 +0100

    Switched options to use FontAwesome icons for modules.

---
+++ Cajetan.Infobar/Converters/ModuleTypeToFontAwesomeIconConverter.cs
@@ -0,0 +1,30 @@
+﻿using Cajetan.Infobar.Domain.Models;
+using FontAwesome5;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cajetan.Infobar.Converters
+{
+    public class ModuleTypeToFontAwesomeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not EModuleType moduleType) 
+                return value;
+
+            return moduleType switch
+            {
+                EModuleType.Uptime => EFontAwesomeIcon.Solid_Clock,
+                EModuleType.BatteryStatus => EFontAwesomeIcon.Solid_BatteryThreeQuarters,
+                EModuleType.ProcessorUsage => EFontAwesomeIcon.Solid_Microchip,
+                EModuleType.MemoryUsage => EFontAwesomeIcon.Solid_Memory,
+                EModuleType.NetworkUsage => EFontAwesomeIcon.Solid_NetworkWired,
+                _ => EFontAwesomeIcon.Solid_Question
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ Cajetan.Infobar/Converters/ModuleTypeToFontAwesomeRotationConverter.cs
@@ -0,0 +1,21 @@
+﻿using Cajetan.Infobar.Domain.Models;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cajetan.Infobar.Converters
+{
+    public class ModuleTypeToFontAwesomeRotationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not EModuleType moduleType) 
+                return value;
+
+            return moduleType == EModuleType.BatteryStatus ? 270 : 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
